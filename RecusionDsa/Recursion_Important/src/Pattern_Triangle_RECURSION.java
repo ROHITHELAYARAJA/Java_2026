f822d64d524a53054e9967b752714c7a89410c91
ROHITHELAYARAJA <e.rohith3130@gmail.com>
2026-06-16 10:36:23 +0530

    Two triangle pattern wise problem in recursion

--- RecusionDsa/Recursion_Important/src/Pattern_Triangle_RECURSION.java
+++ RecusionDsa/Recursion_Important/src/Pattern_Triangle_RECURSION.java
@@ -1,6 +1,8 @@
 public class Pattern_Triangle_RECURSION {
     public static void main(String[] args) {
         Triangle(4,0);
+
+        Triangle2(4,0);
     }
     static void Triangle(int row,int coloumn){
         if(row==0){
@@ -13,6 +15,19 @@ public class Pattern_Triangle_RECURSION {
         else{
             System.out.println();
             Triangle(row-1,0);
+        }
+    }
+    static void Triangle2(int row,int coloumn){
+        if(row==0){
+            return;
+        }
+        if(coloumn<row){
+            Triangle2(row,coloumn+1);
+            System.out.print("@");
+        }
+        else{
+            Triangle2(row-1,0);
+            System.out.println();
         }
     }
 }
