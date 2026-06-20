cf6403b5a4f7bda049417381afee3adda000ae25
ROHITHELAYARAJA <e.rohith3130@gmail.com>
2026-06-20 10:49:45 +0530

    Important problem

--- RecusionDsa/Recursion_Important/Recursion_Pattern_Problem/src/String_Pattern_FAANG.java
+++ RecusionDsa/Recursion_Important/Recursion_Pattern_Problem/src/String_Pattern_FAANG.java
@@ -1,6 +1,8 @@
 public class String_Pattern_FAANG {
     static void main() {
         skip("","baabcs");
+        System.out.println(skip("baabcs"));
+        System.out.println(skipBadam("I LOVE YOU  badam"));
     }
     static void skip(String p,String up){
         if(up.isEmpty()){
@@ -13,6 +15,29 @@ public class String_Pattern_FAANG {
         }
         else{
             skip(p+ch,up.substring(1));
+        }
+    }
+    static String skip(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+        char ch = up.charAt(0);
+        if(ch=='a'){
+           return skip(up.substring(1));
+        }
+        else{
+            return ch+skip(up.substring(1));
+        }
+    }
+    static String skipBadam(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+       if(up.startsWith("badam")){
+            return skipBadam(up.substring(5));
+        }
+        else{
+            return up.charAt(0)+skipBadam(up.substring(1));
         }
     }
 }
