686c9b76676587c70d97cbbc5355609195ddd337
ROHITHELAYARAJA <e.rohith3130@gmail.com>
2026-06-28 20:12:23 +0530

    This is contain the all example and codes of the singly,doubly,circular linked list in java Imporatnt

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -1,4 +1,62 @@
-package PACKAGE_NAME;
-
 public class Main {
+    static void main() {
+//        LL list = new LL();
+//        list.insertFirst(5);
+//        list.insertFirst(6);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(0);
+//        list.insertFirst(1);
+//        list.insertFirst(2);
+//        list.insertFirst(3);
+//        list.display();
+//        System.out.println();
+//        System.out.println(list.getClass());
+//        list.insertLast(101);
+//        list.display();
+//        System.out.println();
+//        list.insert(222,2);
+//        list.display();
+//        System.out.println();
+////        System.out.println("Exception error");
+////        list.insert(78,90);
+////        list.display();
+//
+//        System.out.println(list.deleteFirst());
+//        list.display();
+//        System.out.println();
+//        System.out.println(list.deleteLast());
+//        list.display();
+//        System.out.println();
+//        System.out.println(list.delete(1));
+//        list.display();
+//        System.out.println();
+//
+//        DLL list = new DLL();
+//        list.insertFirst(5);
+//        list.insertFirst(6);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(0);
+//        list.insertFirst(1);
+//        list.insertFirst(2);
+//        list.insertFirst(3);
+//        list.insertLast(690);
+//        list.display();
+//        System.out.println();
+//        list.insert(5,235);
+//        list.display();
+
+        CLL list = new CLL();
+        list.insert(2);
+        list.insert(4);
+        list.insert(7);
+        list.insert(24);
+        list.insert(85);
+        list.insert(98);
+        list.display();
+        System.out.println();
+        list.delete(24);
+        list.display();
+    }
 }
